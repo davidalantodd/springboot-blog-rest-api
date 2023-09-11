d315e04046170500ae2f7f99d945f6a01a287121
davidalantodd <david.todd@multiverse.io>
2023-09-11 06:49:07 -0400

    add PostMapping annotation to PostController and fix error

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class PostController {
     }
 
     // create blog post (DTO as parameter, entity as response)
+    @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
-        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED)
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 }
