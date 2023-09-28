c9f36efbceddaf107ca9442de1caf27cacdac2a3
davidalantodd <david.todd@multiverse.io>
2023-09-28 07:32:52 -0400

    add ability to sort in ascending or descending order for GET /posts

--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -40,9 +40,12 @@ public class PostServiceImpl implements PostService {
 
     // add pagination functionality
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+        //create sort object with sort direction depending on whether sortDir is asc or desc
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+
         //create Pageable instance, include pagination and sorting arguments
-        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
 
         // use postRepository to find all posts, include pagination
         Page<Post> posts = postRepository.findAll(pageable);
