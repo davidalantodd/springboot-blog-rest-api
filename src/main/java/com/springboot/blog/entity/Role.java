61aa0d495d7a41877b1afad557347f65f97a3031
davidalantodd <david.todd@multiverse.io>
2024-05-08 17:49:59 -0400

    create User and Role JPA entities and establish many-to-many relationship

---
+++ src/main/java/com/springboot/blog/entity/Role.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
